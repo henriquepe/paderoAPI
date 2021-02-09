acdff32ffc6a2be0a873a567795c5cc9400524da
Henrique Pires <henriquepe@Henriques-MacBook-Pro.local>
2021-02-09 08:25:09 -0300

    padero api

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/padero_db', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://rosa1702:rosa1702@paderodb.m7oe2.mongodb.net/paderoDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
